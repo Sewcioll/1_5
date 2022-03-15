3e2b40b5f76738ab6f6926a15209f3e63f5c859f
Sewcioll <100691797+Sewcioll@users.noreply.github.com>
2022-03-15 12:33:49 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Liczby parzyste w zakresie do 100");
+    for(int i=0;i<100;i++)
+      {
+        if(i%2==0)
+        {
+          System.out.println(" "+i);
+        }
+      }
+  }
+}
